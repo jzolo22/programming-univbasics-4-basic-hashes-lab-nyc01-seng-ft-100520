99b681dfef1262b88f80c3224f976f3688dad0ce
Julia Zolotarev <jzolotarev@gmail.com>
2020-09-10 17:16:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -13,11 +13,12 @@ end
 def pioneer
   pioneer = {
     :name => "Grace Hopper"
-    :
   }
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  id_hash_generator = {
+    :id => number
+  }
 end
